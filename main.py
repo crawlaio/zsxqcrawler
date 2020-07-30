4305a66d400f72125cc0971bf813ab8ff72755c2
shitao <133397418@qq.com>
2020-07-31 07:44:00 +0800

    添加结果数据

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class ZsxqSpider(object):
     def __init__(self):
-        self.group_id = "152********812"
+        # self.group_id = "152********812"
+        # self.cookies = {
+        #     "UM_distinctid": "********",
+        #     "abtest_env": "product",
+        #     "zsxq_access_token": "********",
+        #     "sensorsdata2015jssdkcross": "********",
+        # }
+        self.group_id = "88512428581812"
         self.cookies = {
-            "UM_distinctid": "********",
-            "abtest_env": "product",
-            "zsxq_access_token": "********",
-            "sensorsdata2015jssdkcross": "********",
+            'UM_distinctid': '1739fb0c72a48d-0cb68d0f731622-31677305-1ea000-1739fb0c72bf18',
+            'abtest_env': 'product',
+            'zsxq_access_token': '3FDC1F86-784C-FA56-FA61-A6982F85A4B5_DCB1ECD2A5B8202E',
+            'sensorsdata2015jssdkcross': '%7B%22distinct_id%22%3A%221739fb37f8e2c-0801e419f76d5e-31677305-2007040-1739fb37f8f968%22%2C%22%24device_id%22%3A%221739fb37f8e2c-0801e419f76d5e-31677305-2007040-1739fb37f8f968%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D',
         }
-
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -80,18 +86,20 @@ class ZsxqSpider(object):
     def parse_html(content):
         content = content.replace("\n", "<br>")
         result = re.findall(r"<e [^>]*>", content)
-        for i in result:
-            html = PyQuery(i)
-            if html.attr("type") == "web":
-                template = "[%s](%s)" % (parse.unquote(html.attr("title")), parse.unquote(html.attr("href")))
-            elif html.attr("type") == "hashtag":
-                template = " `%s` " % parse.unquote(html.attr("title"))
-            elif html.attr("type") == "mention":
-                template = parse.unquote(html.attr("title"))
-            else:
-                template = i
-            content = content.strip().replace(i, template)
-        content = pangu.spacing_text(content)
+        if result:
+            for i in result:
+                html = PyQuery(i)
+                if html.attr("type") == "web":
+                    template = "[%s](%s)" % (parse.unquote(html.attr("title")), parse.unquote(html.attr("href")))
+                elif html.attr("type") == "hashtag":
+                    template = " `%s` " % parse.unquote(html.attr("title"))
+                elif html.attr("type") == "mention":
+                    template = parse.unquote(html.attr("title"))
+                else:
+                    template = i
+                content = content.strip().replace(i, template)
+        else:
+            content = pangu.spacing_text(content)
         return content
 
     @staticmethod
