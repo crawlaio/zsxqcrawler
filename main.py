f06fa016b488d8b464d7e6d91f5208d46d5f05bd
shitao <133397418@qq.com>
2020-07-31 07:52:01 +0800

    添加结果数据

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class ZsxqSpider(object):
             "zsxq_access_token": "********",
             "sensorsdata2015jssdkcross": "********",
         }
+
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
